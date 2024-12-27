d6b05725c9fb37d97e5e319c498c1e89e0218341
Matthew Bitz <mlb21@rice.edu>
2024-12-26 20:20:19 -0600

    Added hyperlinks to the navbar sections

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('base.html')
+
+@app.route('/<section>')
+def genres(section):
+    return render_template('search_section.html', section=section)
